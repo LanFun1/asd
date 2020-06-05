070a179a8d590ef9baafb607b94a41812f4d77c7
LanFun1 <66485374+LanFun1@users.noreply.github.com>
2020-06-05 21:55:10 +0300

    first

---
+++ Assets/Scripts/EnemiesSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesSpawner : MonoBehaviour
+{
+    [SerializeField] public SpriteRenderer _enemyPrefab;
+    [SerializeField] private Transform[] _points;
+    private Vector2 _enemyPos;
+    private int _ch;
+    private float _timer = 2f;
+
+    private void Update()
+    {
+        _timer -= Time.deltaTime;
+        if(_timer <= 0 )
+        {
+            _timer = 2f;
+            StartCoroutine(SpawnIn());
+        }
+    }
+
+    private IEnumerator SpawnIn()
+    {       
+        RandomPos();
+        Instantiate(_enemyPrefab, _enemyPos, Quaternion.identity);
+        yield return null;
+    }
+    private void RandomPos()
+    {
+        _ch = Random.Range(0, _points.Length);
+        _enemyPos = _points[_ch].transform.position;
+    }
+}
