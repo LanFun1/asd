12de2eb572d7ae8b9ad508b67fc55612fbd596a9
LanFun1 <66485374+LanFun1@users.noreply.github.com>
2020-06-07 19:19:27 +0300

    hw

--- Assets/Scripts/EnemiesSpawner.cs
+++ Assets/Scripts/EnemiesSpawner.cs
@@ -7,28 +7,27 @@ public class EnemiesSpawner : MonoBehaviour
     [SerializeField] public SpriteRenderer _enemyPrefab;
     [SerializeField] private Transform[] _points;
     private Vector2 _enemyPos;
-    private int _ch;
-    private float _timer = 2f;
+    private int _number;
+    private bool _courChecker = true;
 
     private void Update()
     {
-        _timer -= Time.deltaTime;
-        if(_timer <= 0 )
-        {
-            _timer = 2f;
+        if(_courChecker == true)
             StartCoroutine(SpawnIn());
-        }
+        _courChecker = false;
     }
 
     private IEnumerator SpawnIn()
-    {       
+    {
+        var waitforsec = new WaitForSeconds(2f);
         RandomPos();
         Instantiate(_enemyPrefab, _enemyPos, Quaternion.identity);
-        yield return null;
+        yield return waitforsec;
+        _courChecker = true;
     }
     private void RandomPos()
     {
-        _ch = Random.Range(0, _points.Length);
-        _enemyPos = _points[_ch].transform.position;
+        _number = Random.Range(0, _points.Length);
+        _enemyPos = _points[_number].transform.position;
     }
 }
